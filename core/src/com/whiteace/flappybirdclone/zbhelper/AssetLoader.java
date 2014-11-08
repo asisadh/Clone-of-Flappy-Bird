337a050010d233f39998686d6d827018dbacc7e3
Aashish <asis.adh@gmail.com>
2014-11-08 22:14:30 +0545

    Flappy Bird Version 2

--- core/src/com/whiteace/flappybirdclone/zbhelper/AssetLoader.java
+++ core/src/com/whiteace/flappybirdclone/zbhelper/AssetLoader.java
@@ -1,6 +1,7 @@
 package com.whiteace.flappybirdclone.zbhelper;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
@@ -10,67 +11,96 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class AssetLoader {
 
-    public static Texture texture;
-    public static TextureRegion bg, grass;
+        public static Texture texture, logoTexture;
+        public static TextureRegion logo, zbLogo, bg, grass, bird, birdDown,
+                birdUp, skullUp, skullDown, bar, playButtonUp, playButtonDown;
+        public static Animation birdAnimation;
+        public static Sound dead, flap, coin;
+        public static BitmapFont font, shadow;
+        private static Preferences prefs;
 
-    public static Animation birdAnimation;
-    public static TextureRegion bird, birdDown, birdUp;
+        public static void load() {
 
-    public static TextureRegion skullUp, skullDown, bar;
-    public static Sound dead, flap, coin;
-    public static BitmapFont font, shadow;
+            logoTexture = new Texture(Gdx.files.internal("data/logo.png"));
+            logoTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 
-    public static void load() {
+            logo = new TextureRegion(logoTexture, 0, 0, 512, 114);
 
-        texture = new Texture(Gdx.files.internal("data/texture.png"));
-        texture.setFilter(TextureFilter.Nearest, TextureFilter.Nearest);
+            texture = new Texture(Gdx.files.internal("data/texture.png"));
+            texture.setFilter(TextureFilter.Nearest, TextureFilter.Nearest);
 
-        bg = new TextureRegion(texture, 0, 0, 136, 43);
-        bg.flip(false, true);
+            playButtonUp = new TextureRegion(texture, 0, 83, 29, 16);
+            playButtonDown = new TextureRegion(texture, 29, 83, 29, 16);
+            playButtonUp.flip(false, true);
+            playButtonDown.flip(false, true);
 
-        grass = new TextureRegion(texture, 0, 43, 143, 11);
-        grass.flip(false, true);
+            zbLogo = new TextureRegion(texture, 0, 55, 135, 24);
+            zbLogo.flip(false, true);
 
-        birdDown = new TextureRegion(texture, 136, 0, 17, 12);
-        birdDown.flip(false, true);
+            bg = new TextureRegion(texture, 0, 0, 136, 43);
+            bg.flip(false, true);
 
-        bird = new TextureRegion(texture, 153, 0, 17, 12);
-        bird.flip(false, true);
+            grass = new TextureRegion(texture, 0, 43, 143, 11);
+            grass.flip(false, true);
 
-        birdUp = new TextureRegion(texture, 170, 0, 17, 12);
-        birdUp.flip(false, true);
+            birdDown = new TextureRegion(texture, 136, 0, 17, 12);
+            birdDown.flip(false, true);
 
-        TextureRegion[] birds = { birdDown, bird, birdUp };
-        birdAnimation = new Animation(0.06f, birds);
-        birdAnimation.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
+            bird = new TextureRegion(texture, 153, 0, 17, 12);
+            bird.flip(false, true);
 
-        skullUp = new TextureRegion(texture, 192, 0, 24, 14);
-        
-        // Create by flipping existing skullUp
-        skullDown = new TextureRegion(skullUp);
-        skullDown.flip(false, true);
+            birdUp = new TextureRegion(texture, 170, 0, 17, 12);
+            birdUp.flip(false, true);
 
-        bar = new TextureRegion(texture, 136, 16, 22, 3);
-        bar.flip(false, true);
+            TextureRegion[] birds = { birdDown, bird, birdUp };
+            birdAnimation = new Animation(0.06f, birds);
+           // birdAnimation.setPlayMode(Animation.LOOP_PINGPONG);
 
-        dead = Gdx.audio.newSound(Gdx.files.internal("data/dead.wav"));
-        flap = Gdx.audio.newSound(Gdx.files.internal("data/flap.wav"));
-        coin = Gdx.audio.newSound(Gdx.files.internal("data/coin.wav"));
-        
-        font = new BitmapFont(Gdx.files.internal("data/text.fnt"));
-        font.setScale(.25f, -.25f);
-        shadow = new BitmapFont(Gdx.files.internal("data/shadow.fnt"));
-        shadow.setScale(.25f, -.25f);
-    }
+            skullUp = new TextureRegion(texture, 192, 0, 24, 14);
+            // Create by flipping existing skullUp
+            skullDown = new TextureRegion(skullUp);
+            skullDown.flip(false, true);
 
-    public static void dispose() {
-        // We must dispose of the texture when we are finished.
-        texture.dispose();
-        dead.dispose();
-        flap.dispose();
-        coin.dispose();
-        font.dispose();
-        shadow.dispose();
-    }
+            bar = new TextureRegion(texture, 136, 16, 22, 3);
+            bar.flip(false, true);
+
+            dead = Gdx.audio.newSound(Gdx.files.internal("data/dead.wav"));
+            flap = Gdx.audio.newSound(Gdx.files.internal("data/flap.wav"));
+            coin = Gdx.audio.newSound(Gdx.files.internal("data/coin.wav"));
+
+            font = new BitmapFont(Gdx.files.internal("data/text.fnt"));
+            font.setScale(.25f, -.25f);
+            shadow = new BitmapFont(Gdx.files.internal("data/shadow.fnt"));
+            shadow.setScale(.25f, -.25f);
+
+            // Create (or retrieve existing) preferences file
+            prefs = Gdx.app.getPreferences("ZombieBird");
+
+            if (!prefs.contains("highScore")) {
+                prefs.putInteger("highScore", 0);
+            }
+        }
+
+        public static void setHighScore(int val) {
+            prefs.putInteger("highScore", val);
+            prefs.flush();
+        }
+
+        public static int getHighScore() {
+            return prefs.getInteger("highScore");
+        }
+
+        public static void dispose() {
+            // We must dispose of the texture when we are finished.
+            texture.dispose();
+
+            // Dispose sounds
+            dead.dispose();
+            flap.dispose();
+            coin.dispose();
+
+            font.dispose();
+            shadow.dispose();
+        }
 
 }
